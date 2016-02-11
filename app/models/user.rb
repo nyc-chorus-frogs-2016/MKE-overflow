073eee4532b35f8d4cabd361f60cb41e29c47634
EdwardGemson <egemson@skidmore.edu>
2016-02-11 17:12:39 -0500

    complete user login and registration

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+	has_secure_password
+	has_many :questions
+	has_many :answers
+	has_many :votes
+	has_many :comments 
+	validates :username, presence: true, uniqueness: true
+
+end
