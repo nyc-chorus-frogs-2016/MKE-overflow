1a88edd5a7f58ce7bdebf39b64054793ae99af90
EdwardGemson <egemson@skidmore.edu>
2016-02-11 18:15:32 -0500

    complete seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+  User.create!(username: Faker::Internet.user_name, password: "123")
+end
+
+15.times do 
+	Question.create!(title: Faker::Name.title, content: Faker::Hipster.paragraph, user_id: User.all.sample)
+end
+
+15.times do 
+	Answer.create!(content: Faker::Hipster.paragraph, question_id: Question.all.sample, user_id: User.all.sample)
+end
