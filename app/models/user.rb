f340f85d508084ade3ffc0674ce25ce29ffb2072
Edward <email": null>
2016-02-12 16:39:56 -0500

    Complete voting on answers and user show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,15 @@ class User < ActiveRecord::Base
 	has_many :comments
 	validates :username, presence: true, uniqueness: true
 
-
   def has_voted_question?(question)
     votes.where(votable_id: question.id, votable_type: 'Question').count > 0
   end
 
   def has_voted_answer?(answer)
     votes.where(votable_id: answer.id, votable_type: 'Answer').count > 0
+  end
 
   def has_voted?(question)
     Question.votes.where(user: current_user).count > 0
-
   end
 end
